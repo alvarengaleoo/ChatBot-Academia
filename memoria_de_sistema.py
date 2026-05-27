e142a47cfdd84879cb43bafe5226f28b8d6c7121
Leonardo Alvarenga <alvarengaleoo@gmail.com>
2026-05-27 14:04:35 -0300

    estruturando

--- memoria_de_sistema.py
+++ memoria_de_sistema.py
@@ -7,4 +7,4 @@ def get_session_history(session_id):
     return SQLChatMessageHistory(session_id, connection="sqlite:///memory.db")
 
 # criando a função que corta o historico e captura as 10 ultimas mensagens trocadas na conversa:
-trimmeer = trim_messages(strategy="last", max_tokens=10, token_counter=len)
+trimmer = trim_messages(strategy="last", max_tokens=10, token_counter=len)
